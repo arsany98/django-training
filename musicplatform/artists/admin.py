9cba99a23ab9d74d2de5f63973886e2c57126c06
arsany98 <arsanymaged13@gmail.com>
2022-10-22 23:00:34 +0200

    fix: Edit AlbumsInline

--- musicplatform/artists/admin.py
+++ musicplatform/artists/admin.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
 
 from albums.models import Album
+from albums.forms import AlbumForm
 
 from .models import Artist
 
 class AlbumsInline(admin.TabularInline):
     model = Album
+    fields = ('artist', 'name', 'creation_datetime',
+              'release_datetime', 'cost', 'is_approved')
+    readonly_fields = ('creation_datetime',)
+    form = AlbumForm
+    extra = 1
+    
 @admin.register(Artist)
 class ArtistAdmin(admin.ModelAdmin):
     list_display = ('stage_name', 'get_num_of_approved_albums')
