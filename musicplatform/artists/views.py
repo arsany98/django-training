90c8308cbb8d79094e5883beea9b0feb3db820af
arsany98 <arsanymaged13@gmail.com>
2022-10-30 23:13:33 +0200

    Add User Authentication - Implement login required mixin on create views - Create login template - Create base site template - Add all authentication templates - Redirect to artists on login

--- musicplatform/artists/views.py
+++ musicplatform/artists/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import CreateView, ListView
-
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import ArtistForm
 
 from .models import Artist
 
 
-class ArtistCreateView(CreateView):
+class ArtistCreateView(LoginRequiredMixin, CreateView):
     model = Artist
     form_class = ArtistForm
     template_name = 'artists/create.html'
