65a1322745b466d4fcf984d8d25022c21d64e1a1
arsany98 <arsanymaged13@gmail.com>
2022-11-07 22:06:41 +0200

    Create django filters - Create cost__gte, cost__lte, name filters using django filters - Add filterset class to album list view - Create /albums/filter/ endpoint - Create Album List Filters View - Override get queryset to filter queryset with cost__gte, cost__lte and name__icontains

--- musicplatform/albums/views.py
+++ musicplatform/albums/views.py
@@ -4,6 +4,9 @@ from rest_framework.response import Response
 from .models import Album
 from .serializers import AlbumSerializer
 from .permissions import IsArtistOrReadOnly
+from django_filters import rest_framework as filters
+from .filters import AlbumFilter
+from rest_framework.validators import ValidationError
 
 
 class AlbumListView(generics.ListCreateAPIView):
@@ -11,6 +14,8 @@ class AlbumListView(generics.ListCreateAPIView):
         permissions.IsAuthenticatedOrReadOnly, IsArtistOrReadOnly]
     queryset = Album.approved_albums.prefetch_related('artist')
     serializer_class = AlbumSerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_class = AlbumFilter
 
     def post(self, request, *args, **kwargs):
         serializer = AlbumSerializer(data=request.data)
@@ -19,3 +24,25 @@ class AlbumListView(generics.ListCreateAPIView):
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors)
+
+
+class AlbumListFiltersView(generics.ListAPIView):
+    serializer_class = AlbumSerializer
+
+    def get_queryset(self):
+
+        queryset = Album.approved_albums.prefetch_related('artist')
+        cost__gte = self.request.query_params.get('cost__gte')
+        if cost__gte is not None:
+            if not cost__gte.isnumeric():
+                raise ValidationError('Enter a number.')
+            queryset = queryset.filter(cost__gte=cost__gte)
+        cost__lte = self.request.query_params.get('cost__lte')
+        if cost__lte is not None:
+            if not cost__lte.isnumeric():
+                raise ValidationError('Enter a number.')
+            queryset = queryset.filter(cost__lte=cost__lte)
+        name__icontains = self.request.query_params.get('name')
+        if name__icontains is not None:
+            queryset = queryset.filter(name__icontains=name__icontains)
+        return queryset
--- musicplatform/musicplatform/settings.py
+++ musicplatform/musicplatform/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'django_extensions',
     'users',
     'knox',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
